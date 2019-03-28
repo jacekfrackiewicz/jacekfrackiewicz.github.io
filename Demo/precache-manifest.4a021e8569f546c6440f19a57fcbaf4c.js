d5b89244028cc9b3c3b128ed5fde95f10c32d226
jacekfrackiewicz <jacek.frack@gmail.com>
2019-03-28 22:45:20 +0100

    Demo site 1

---
+++ Demo/precache-manifest.4a021e8569f546c6440f19a57fcbaf4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11db2e845c757ed91a722bad1b399289",
+    "url": "/static/media/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.11db2e84.woff2"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "991abe37dfa94acc4b69",
+    "url": "/static/js/main.9273e567.chunk.js"
+  },
+  {
+    "revision": "7bd40ca14c49b88850ac",
+    "url": "/static/js/2.97f5c88f.chunk.js"
+  },
+  {
+    "revision": "991abe37dfa94acc4b69",
+    "url": "/static/css/main.31930c0e.chunk.css"
+  },
+  {
+    "revision": "59e9794280a851b482052c15f8626ddb",
+    "url": "/index.html"
+  }
+];
