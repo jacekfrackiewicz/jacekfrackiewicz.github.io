f5a5daa942117c37fe7fa9a5388ce2badb35b848
jacekfrackiewicz <jacek.frack@gmail.com>
2019-10-01 01:00:54 +0200

    Sebastian-kartka

---
+++ Sebastian/precache-manifest.9ef7e8d53326b0aa2c2b67ac2dd3f884.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e533121d3aefecc994fa2f656a84f563",
+    "url": "./index.html"
+  },
+  {
+    "revision": "24cfcbb875b13d7da9df",
+    "url": "./static/css/main.37474ed6.chunk.css"
+  },
+  {
+    "revision": "cbf04820f05c40f66c67",
+    "url": "./static/js/2.a99f2c57.chunk.js"
+  },
+  {
+    "revision": "24cfcbb875b13d7da9df",
+    "url": "./static/js/main.d04bbedd.chunk.js"
+  },
+  {
+    "revision": "9a919cab6c4a9fb0639f",
+    "url": "./static/js/runtime-main.b9619071.js"
+  },
+  {
+    "revision": "13d7726bf2e6f1dbe94901a6f29dd01c",
+    "url": "./static/media/wendy-one-v8-latin-ext_latin-regular.13d7726b.svg"
+  },
+  {
+    "revision": "16a7d68427939b28d6d75955640d2c51",
+    "url": "./static/media/wendy-one-v8-latin-ext_latin-regular.16a7d684.eot"
+  },
+  {
+    "revision": "d43a64e3bc8d871ac46201357d33d4f2",
+    "url": "./static/media/wendy-one-v8-latin-ext_latin-regular.d43a64e3.ttf"
+  },
+  {
+    "revision": "d8d50aa30f8682808aeb46cbf2d14a14",
+    "url": "./static/media/wendy-one-v8-latin-ext_latin-regular.d8d50aa3.woff"
+  },
+  {
+    "revision": "fa905ef5a7cb7f0247a12de43927093a",
+    "url": "./static/media/wendy-one-v8-latin-ext_latin-regular.fa905ef5.woff2"
+  }
+]);
